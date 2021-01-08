0aa60392ec33548589f24e76ea7d6bcde9532878
tester-404 <test404bugs@gmail.com>
2021-01-07 17:09:05 -0800

    ok

--- src/test/java/RestAssured4.java
+++ src/test/java/RestAssured4.java
@@ -2,10 +2,13 @@ import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class RestAssured4 {
     @Test
-    public void Test01(){
+    public void Test01() {
         Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 
         System.out.println(response.getStatusCode());
@@ -16,6 +19,20 @@ public class RestAssured4 {
 
         int statusCode = response.getStatusCode();
         assertEquals(statusCode, 200);
-
     }
+
+    @Test
+    public void Test02(){
+        RestAssured.
+                baseURI="https://reqres.in/api";
+        RestAssured.
+                given().
+                get("/user?page=2").
+                then().
+                statusCode(200).
+                body("data[1].id",equalTo(8))
+                .log().all();
+        }
+
+
 }
